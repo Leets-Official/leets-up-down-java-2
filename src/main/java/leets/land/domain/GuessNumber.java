f56e7e11afad6bdc215dbaf4c6c1b205dbbbe6e5
ay-eonii <naayen@naver.com>
2024-04-13 16:09:23 +0900

    feat(GuessNumber): 추측값 도메인

---
+++ src/main/java/leets/land/domain/GuessNumber.java
@@ -0,0 +1,21 @@
+package leets.land.domain;
+
+public class GuessNumber {
+
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 100;
+    private static final String ERROR_FORMAT = "[ERROR] %s";
+
+    private final int value;
+
+    public GuessNumber(int value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(int value) {
+        if (value < MIN_RANGE || value > MAX_RANGE) {
+            throw new IllegalArgumentException(String.format(ERROR_FORMAT, "추측값의 범위는 1 이상 100 이하입니다"));
+        }
+    }
+}
