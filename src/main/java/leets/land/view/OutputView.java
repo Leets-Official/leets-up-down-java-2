d9f9292a98fbe0e6a010a95393f53a31e478434e
rootTiket <l20078829@gmail.com>
2024-04-13 17:55:22 +0900

    [feat] : 정답일때 시도횟수를 출력하는 함수 추가

---
+++ src/main/java/leets/land/view/OutputView.java
@@ -0,0 +1,9 @@
+package leets.land.view;
+
+public class OutputView {
+    public void printCorrectMessage(int cnt) {
+        System.out.println("정답");
+
+        System.out.printf("시도한 횟수 : %d회",cnt);
+    }
+}
