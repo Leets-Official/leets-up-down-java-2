0fb7d0fa3bca5e34b66c9835d6d31e6731e56ead
KoungQ <guu119@naver.com>
2024-04-15 17:55:52 +0900

    REFACTOR: validator의 리턴 타입 변경

--- src/main/java/leets/land/validation/validator/VersionValidator.java
+++ src/main/java/leets/land/validation/validator/VersionValidator.java
@@ -2,14 +2,17 @@ package leets.land.validation.validator;
 
 import leets.land.validation.constraint.Constraint;
 import leets.land.validation.constraint.InvalidVersionConstraint;
+import leets.land.validation.exception.InvalidInputException;
 
 import java.util.List;
 
 public class VersionValidator {
 
-    private final List<Constraint<Integer>> constraints = List.of(new InvalidVersionConstraint());  // 다른 제약 조건이 생기면 추가
+    private final List<Constraint<String>> constraints = List.of(new InvalidVersionConstraint());  // 다른 제약 조건이 생기면 추가
 
-    public boolean validate(int value) {
-        return constraints.stream().allMatch(constraint -> constraint.isValid(value));
+    public void validate(Object value) throws InvalidInputException {
+        for (Constraint<String> constraint : constraints) {
+            constraint.isValid(value.toString());
+        }
     }
 }
