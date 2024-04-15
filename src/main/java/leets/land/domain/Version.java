f49aea680721f3e709886ebcaac6b7b2a4065a54
KoungQ <guu119@naver.com>
2024-04-15 14:29:07 +0900

    FEAT: 출력을 위한 객체 추가

--- src/main/java/leets/land/domain/Version.java
+++ src/main/java/leets/land/domain/Version.java
@@ -1,16 +1,22 @@
 package leets.land.domain;
 
 public enum Version {
-    ENGLISH_VERSION(1),
-    NUMBER_VERSION(2);
+    ENGLISH_VERSION(1, "영어"),
+    NUMBER_VERSION(2, "숫자");
 
     private final int value;
+    private final String name;
 
-    Version(int value) {
+    Version(int value, String name) {
         this.value = value;
+        this.name = name;
     }
 
     public int getValue() {
         return value;
+    }
+
+    public String getName() {
+        return name;
     }
 }
