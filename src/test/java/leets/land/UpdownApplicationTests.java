7d643f44fef10d18ff69827fd31c4be77a8ec610
jj0526 <wjstlgus1@naver.com>
2024-04-16 15:42:40 +0900

    기능 구현 완료

--- src/test/java/leets/land/UpdownApplicationTests.java
+++ src/test/java/leets/land/UpdownApplicationTests.java
@@ -1,11 +1,30 @@
 package leets.land;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UpdownApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+class UpdownApplicationTests {
+	UpdownApplication app;
+
+	@BeforeEach
+	void setUp() {
+		app = new UpdownApplication();
+	}
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void checkNumberValidity(){
+		int min = 40;
+		int max = 60;
+		int userInput = 20;
+
+		assertThrows(Exception.class, ()->{
+			app.checkNumberValidity(min, max, userInput);
+		});
+
 	}
 
 }
