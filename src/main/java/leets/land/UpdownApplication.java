8987d551137bc2e16f1105fb051a3280aab5de35
hyxklee <ewgt1234@naver.com>
2024-04-16 16:06:26 +0900

    요구사항 분석

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,8 +1,43 @@
 package leets.land;
 
-public class UpdownApplication {
+import java.util.Random;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.print("hihi :D");
+public class UpdownApplication {
+	Random random = new Random();
+	Scanner scan = new Scanner(System.in);
+	//실행부
+	public void run() {
+		//버전 입력
+		int version = scan.nextInt();
+		//정답 입력
+
+		//정답 출력
+	}
+
+	//무작위 수 반환
+	public int generateRandomNum(){
+		int randomNum = (random.nextInt(100)+1);
+		return randomNum;
+	}
+
+	//무작위 알파벳 반환
+	public char generateRandomChar() {
+		if(random.nextBoolean()){
+			return (char) (random.nextInt(26) + 65);
+		}
+		else{
+			return (char) (random.nextInt(26) + 97);
+		}
+	}
+
+
+	public static void main(String[] args)  {
+		UpdownApplication app = new UpdownApplication();
+		app.run();
 	}
 }
+
+
+
+
