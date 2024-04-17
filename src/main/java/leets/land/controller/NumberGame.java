2a5f58ee7c168fc32910dd22dae8007ebd5abb8b
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-04-18 00:39:08 +0900

    feat: 숫자 게임 구현

---
+++ src/main/java/leets/land/controller/NumberGame.java
@@ -0,0 +1,44 @@
+package leets.land.controller;
+
+import leets.land.domain.NumberUpDown;
+import leets.land.domain.UpDown;
+import leets.land.view.InputView;
+import leets.land.view.OutputView;
+
+public class NumberGame implements Game{
+    final InputView inputView = new InputView();
+    final OutputView outputView = new OutputView();
+
+    @Override
+    public void run() {
+        UpDown updown = new NumberUpDown();
+        play(updown);
+    }
+
+    @Override
+    public void play(UpDown updown) {
+        while (true) {
+            int number = inputView.inputNumberInRange(updown.getStartAsInt(), updown.getEndAsInt());
+
+            try{
+                updown.validator(number);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+
+            updown.increaseCount();
+
+            if (number == updown.getAnswerAsInt()) {
+                outputView.printCount(updown.getCount());
+                break;
+            } else if (number < updown.getAnswerAsInt()) {
+                System.out.println("UP");
+                updown.setStart(number + 1);
+            } else {
+                System.out.println("DOWN");
+                updown.setEnd(number - 1);
+            }
+        }
+    }
+}
