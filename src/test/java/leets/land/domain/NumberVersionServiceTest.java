88038305bb68f76643b7e4d24230c0fa269a43d5
노정완 <jeongwan.noh@gmail.com>
2024-04-18 03:41:44 +0900

    test: 숫자버전 서비스 테스트 코드 구현

--- src/test/java/leets/land/domain/NumberVersionServiceTest.java
+++ src/test/java/leets/land/domain/NumberVersionServiceTest.java
@@ -1,4 +1,34 @@
+package leets.land.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class NumberVersionServiceTest {
-  
+    private NumberVersionService numberService = new NumberVersionService();
+
+    @BeforeEach
+    void setUp() {
+        numberService = new NumberVersionService();
+    }
+
+    @Test
+    void checkRange() {
+        assertThat(numberService.checkRange("5", 1, 10)).isTrue();
+        assertThat(numberService.checkRange("15", 1, 10)).isFalse();
+    }
+
+    @Test
+    void setRandomNumber() {
+        int randomNum = numberService.setRandomNumber(1, 100);
+        assertThat(randomNum).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(100);
+    }
+
+    @Test
+    void calcIsNumberUp() {
+        assertThat(numberService.calcIsNumberUp(10, 5)).isTrue();
+        assertThat(numberService.calcIsNumberUp(5, 10)).isFalse();
+    }
 }
