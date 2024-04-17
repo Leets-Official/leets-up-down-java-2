fe95b1a4fecad3c2066d503395e9a8a9446c038c
jiixon <jr0107380@gmail.com>
2024-04-18 04:24:23 +0900

    feat: alphabetComputer 구현

--- src/main/java/leets/land/model/AlphabetComputer.java
+++ src/main/java/leets/land/model/AlphabetComputer.java
@@ -1,4 +1,50 @@
 package leets.land.model;
 
-public class Computer {
+public class AlphabetComputer {
+
+    private static char answer = (char) (Math.random() * ('z'-'A'+1) + 'A');
+    private static int count = 1;
+    private static char min = 'A';
+    private static char max = 'z';
+
+
+    public int getCount() {
+        return count;
+    }
+
+    public char getMin(){
+        return min;
+    }
+    public char getMax(){
+        return max;
+    }
+    public void judgeUp(char input) {
+        count++;
+        if (input == 'Z') {
+            min = 'a';
+        } else {
+            max = (char) (input - 1);
+        }
+    }
+
+    public void judgeDown(char input) {
+        count++;
+        if (input == 'a') {
+            max = 'Z';
+        } else {
+            max = (char) (input - 1);
+        }
+    }
+
+    public boolean up(char input) {
+        return answer > input;
+    }
+
+    public boolean down(char input) {
+        return answer < input;
+    }
+
+    public boolean correct(char input) {
+        return answer == input;
+    }
 }
