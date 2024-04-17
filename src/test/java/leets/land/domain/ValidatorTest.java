e02015ec1606bbaf2dc3d55c5b2eefb32bdf24ca
노정완 <jeongwan.noh@gmail.com>
2024-04-18 03:52:38 +0900

    test: Validator 테스트 코드 구현

--- src/test/java/leets/land/domain/ValidatorTest.java
+++ src/test/java/leets/land/domain/ValidatorTest.java
@@ -1,4 +1,50 @@
+package leets.land.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 import static org.junit.jupiter.api.Assertions.*;
+
 class ValidatorTest {
-  
+    private Validator validator = new Validator();
+
+    @BeforeEach
+    void setUp() {
+        validator = new Validator();
+    }
+
+    @Test
+    void versionInputIsValid() {
+        assertThat(validator.versionInputIsValid("1")).isTrue();
+        assertThat(validator.versionInputIsValid("2")).isTrue();
+        assertThat(validator.versionInputIsValid("3")).isFalse();
+    }
+
+    @ValueSource(strings = {"1", "100"})
+    @ParameterizedTest
+    void isInputNumberWithNumber(String input) {
+        assertThat(validator.isInputNumber(input)).isTrue();
+    }
+
+    @ValueSource(strings = {"", " ", "abc"})
+    @ParameterizedTest
+    void isInputNumberWithInvalids(String input) {
+        assertThat(validator.isInputNumber(input)).isFalse();
+    }
+
+    @ValueSource(strings = {"a", "b", "c"})
+    @ParameterizedTest
+    void isInputCharWithChars(String input) {
+        assertThat(validator.isInputChar(input)).isTrue();
+    }
+
+    @ValueSource(strings = {"A", "Z"})
+    @ParameterizedTest
+    void isInputCharWithInvalids(String input) {
+        assertThat(validator.isInputChar(input)).isFalse();
+    }
 }
