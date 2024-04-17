0de3f652979666cfb9ed59ab58880aacc79bb8d8
jiixon <jr0107380@gmail.com>
2024-04-18 00:37:33 +0900

    feat: inputViewm outputView 구현

--- src/main/java/leets/land/view/InputView.java
+++ src/main/java/leets/land/view/InputView.java
@@ -1,4 +1,34 @@
 package leets.land.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final Scanner scanner;
+
+    public InputView(){
+        scanner = new Scanner(System.in);
+    }
+    public int getVersion(){
+        System.out.print("버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2) :");
+        return scanner.nextInt();
+    }
+
+    public int getNumberFirstInput(){
+        System.out.print("숫자를 입력해주세요(1 ~ 100) : ");
+        return scanner.nextInt();
+    }
+
+    public char getAlphabetFirstInput(){
+        System.out.print("영어를 입력해주세요(A ~ z) : ");
+        return scanner.next().charAt(0);
+    }
+
+    public int getNextNumberGuess(int min, int max){
+        System.out.printf("숫자를 입력해주세요(%d ~ %d) : ",min ,max);
+        return scanner.nextInt();
+    }
+    public int getNextAlphabetGuess(char min, char max){
+        System.out.printf("영어를 입력해주세요(%c ~ %c) : ",min ,max);
+        return scanner.next().charAt(0);
+    }
 }
--- src/main/java/leets/land/view/OutputView.java
+++ src/main/java/leets/land/view/OutputView.java
@@ -4,4 +4,12 @@ public class OutputView {
     public void printStart(){
         System.out.println("업다운 게임을 시작합니다.\n");
     }
+
+    public void printUpDownResult(String result){
+        System.out.println(result);
+    }
+
+    public void printAttempt(int attempt){
+        System.out.print("\n시도한 횟수 : " + attempt);
+    }
 }
