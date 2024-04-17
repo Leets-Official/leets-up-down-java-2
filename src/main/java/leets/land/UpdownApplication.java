cce3ddddc5f8d56e5d5c05a30676ac2af0e17aca
anxi01 <tjdals2893@naver.com>
2024-04-17 15:13:45 +0900

    feat : 숫자 버전 view 구현

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,8 +1,15 @@
 package leets.land;
 
+import leets.land.controller.GameController;
+import leets.land.model.GameNumericModel;
+import leets.land.view.GameView;
+
 public class UpdownApplication {
 
 	public static void main(String[] args) {
-		System.out.print("hihi :D");
+		GameView view = new GameView();
+		GameNumericModel numericModel = new GameNumericModel();
+		GameController controller = new GameController(view, numericModel);
+		controller.play();
 	}
 }
---
+++ src/main/java/leets/land/view/GameView.java
@@ -0,0 +1,24 @@
+package leets.land.view;
+
+import java.util.Scanner;
+
+public class GameView {
+
+  public int getVersionInput() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2) : ");
+    return sc.nextInt();
+  }
+
+  public int getNumericInput() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("숫자를 입력해주세요(1 ~ 100) : ");
+    return sc.nextInt();
+  }
+
+  public String getAlphabetInput() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("영어를 입력해주세요(A ~ z) : ");
+    return sc.next();
+  }
+}
