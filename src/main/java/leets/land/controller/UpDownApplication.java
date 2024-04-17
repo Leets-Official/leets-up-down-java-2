4372b8edc0d787fb9cc55413bdf3f5825af01450
KoungQ <guu119@naver.com>
2024-04-17 17:58:40 +0900

    FEAT: 잘못된 값을 5회 이상 입력 시, 프로그램 종료 기능 추가

--- src/main/java/leets/land/controller/UpDownApplication.java
+++ src/main/java/leets/land/controller/UpDownApplication.java
@@ -15,9 +15,17 @@ import static leets.land.domain.Version.NUMBER_VERSION;
 public class UpDownApplication {
 
 	private static final boolean UNTIL_CORRECT = true;
+	private static final int MAX_ERROR_COUNT = 5;
+	private static final int INIT_ERROR_COUNT = 0;
+
 	private static Answer answer;
 	private static final Range range = new Range();
-	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	// 테스트를 위한 Setter
+	public static void setBr(BufferedReader br) {
+		UpDownApplication.br = br;
+	}
 
 	public static void main(String[] args) {
 		UpDownApplication app = new UpDownApplication();
@@ -29,39 +37,49 @@ public class UpDownApplication {
 					업다운 게임을 시작합니다.
 					     
 					버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2):\s""");
-		init();	// Answer 생성
-		guess(); // 게임 시작 + 결과 출력
+		init(INIT_ERROR_COUNT);	// Answer 생성
+		guess(INIT_ERROR_COUNT); // 게임 시작 + 결과 출력
 	}
 
-	public void init() {
+	public void init(int errorCnt) {
 		try {
-			answer = new Answer(read());
+			answer = new Answer(read(errorCnt));
 			initRange();	// 범위 초기화 (숫자면 1~100, 영어면 A~z)
 		} catch (InvalidInputException e) {
 			System.out.print(e.getMessage());
-			init();		// 재귀
+			init(++errorCnt);		// 재귀
 		}
 	}
 
-	public String read() {
+										// 테스트를 위한 throws
+	public String read(int errorCnt) /*throws InvalidInputException*/ {
 		try {
+			if (errorCnt >= MAX_ERROR_COUNT)
+				throw new InvalidInputException("[ERROR] 5회 이상 잘못 입력하셨습니다.\n프로그램을 종료합니다.");
+
 			return Optional.of(br.readLine())
 					.filter(input -> !input.trim().isEmpty())
 					.orElseThrow(() -> new InvalidInputException("[ERROR] 값을 입력해주세요: "));
 		} catch (InvalidInputException e) {
 			System.out.print(e.getMessage());
-			return read();
+
+			if (errorCnt >= MAX_ERROR_COUNT)
+				System.exit(0);
+
+			return read(++errorCnt);
+			// 테스트를 위한 throw
+//			throw new InvalidInputException(e.getMessage());
 		} catch (IOException e) {
 			throw new RuntimeException("[ERROR] 입력을 읽는 중 오류가 발생했습니다: " + e);
 		}
 	}
 
 	// 비즈니스 로직
-	public void guess() {
+	public void guess(int errorCnt) {
 		while (UNTIL_CORRECT) {
 			try {
 				System.out.print(answer.getVersion().getName() + "를 입력해주세요(" + range.getBottom() + " ~ " + range.getTop() +"): ");
-				Trial trial = Trial.getInstance().init(answer.getVersion(), read(), range);
+				Trial trial = Trial.getInstance().init(answer.getVersion(), read(errorCnt), range);
 
 				int result = answer.compareTo(answer.getVersion(), trial.getTrial());	// 조건문의 조건 내에서 연산하면 여러번 수행해야 하므로 따로 뺌
 
@@ -81,6 +99,7 @@ public class UpDownApplication {
 				}
 			} catch (InvalidInputException e) {
 				System.out.println(e.getMessage());
+				++errorCnt;
 			}
 		}
 	}
