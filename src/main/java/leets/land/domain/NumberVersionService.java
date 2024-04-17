a848b2b839f3f29cc0d81778db240712060b0304
노정완 <jeongwan.noh@gmail.com>
2024-04-18 02:13:25 +0900

    feat: 숫자 버전 서비스 로직 구현

--- src/main/java/leets/land/domain/NumberVersionService.java
+++ src/main/java/leets/land/domain/NumberVersionService.java
@@ -1,4 +1,39 @@
 package leets.land.domain;
 
+import java.util.Random;
+
 public class NumberVersionService {
+
+    public int getNumber(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public boolean checkRange(String input, int min, int max) {
+        int value = convertStringToNum(input);
+
+        return value >= min && value <= max;
+    }
+
+    private int convertStringToNum(String input) {
+        return Integer.parseInt(input);
+    }
+
+    public int setRandomNumber(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public boolean checkAnswer(int answer, int number) {
+        if (answer == number) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean calcIsNumberUp(int answer, int number) {
+        if (answer > number) {
+            return true;
+        }
+        return false;
+    }
 }
