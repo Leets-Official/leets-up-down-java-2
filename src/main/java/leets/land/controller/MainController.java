c0dd71ced7d37eaa4f086768e82d80add670b172
노정완 <jeongwan.noh@gmail.com>
2024-04-18 02:13:48 +0900

    feat: 숫자 버전 게임 로직 임시 구현

--- src/main/java/leets/land/controller/MainController.java
+++ src/main/java/leets/land/controller/MainController.java
@@ -10,26 +10,31 @@ import java.io.IOException;
 public class MainController {
     private int gameVersion;
     private final OutputView output = new OutputView();
-    private final InputView input = new InputView();
     private final Validator validator = new Validator();
     private final MainService service = new MainService();
 
     public void run() throws IOException {
+        int tryCount;
         gameStart();
         if (gameVersion == 1) {
-
+            NumberVersionController controller = new NumberVersionController();
+            tryCount = controller.run();
+            System.out.println(tryCount);
+            return;
         }
         if (gameVersion == 2) {
-
+            CharVersionController controller = new CharVersionController();
+            controller.run();
         }
     }
 
     private void gameStart() throws IOException {
+        InputView inputView = new InputView();
         output.showIntro();
 
         while (true) {
             output.showGameVersion();
-            String inputValue = input.getInput();
+            String inputValue = inputView.getInput();
             if (validator.versionInputIsValid(inputValue)) {
                 gameVersion = service.sortVersionInput(inputValue);
                 return;
