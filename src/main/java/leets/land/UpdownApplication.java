f2fd8644c8785789ea36045df64c5a53de45fd6e
jiixon <jr0107380@gmail.com>
2024-04-18 00:24:15 +0900

    feat: application 작성

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,8 +1,11 @@
 package leets.land;
 
+import leets.land.controller.Controller;
+
 public class UpdownApplication {
 
 	public static void main(String[] args) {
-		System.out.print("hihi :D");
+		Controller controller = new Controller();
+		controller.start();
 	}
 }
