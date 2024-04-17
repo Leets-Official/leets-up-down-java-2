c730b02b5f6a32c1832b76118edac7b95663f9ed
노정완 <jeongwan.noh@gmail.com>
2024-04-18 02:12:11 +0900

    feat: 숫자 검증 로직 구현

--- src/main/java/leets/land/domain/Validator.java
+++ src/main/java/leets/land/domain/Validator.java
@@ -1,10 +1,20 @@
 package leets.land.domain;
 
 public class Validator {
+
     public boolean versionInputIsValid(String inputValue) {
         if (inputValue.equals("1") || inputValue.equals("2")) {
             return true;
         }
         return false;
+    }
+
+    public boolean isInputNumber(String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        } catch (NumberFormatException numberFormatException) {
+            return false;
+        }
+        return true;
     }
 }
