8227d2424d986e696ac4fd4394a21617eb5a3d1a
이주애 <joojuae02@gachon.ac.kr>
2024-04-18 17:29:14 +0900

    feat: 요구조건 맞추어 코드 수정 완료

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -4,16 +4,21 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.regex.Pattern;
 
 public class UpdownApplication {
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+	// 난수
+	public static int randNum = (int)(Math.random()*100);
+
+	// 랜덤 단어
+	public static int randChar = (int)(Math.random()*52)+65;
+
 	public static void main(String[] args) throws Exception {
-		// 입력: 숫자 또는 영어
-		// 사용자가 입력하는 숫자, 영어등을 계속 입력
-		// 정답을 맞추게 되면 정답! 메시지를 출력하고 시도한 횟수도 출력
 		int ver = 0; // 버전 저장
+		int cnt = 1; // 시도횟수 저장
 
 		bw.write("업다운 게임을 시작합니다.\n\n");
 
@@ -26,62 +31,96 @@ public class UpdownApplication {
 			bw.write("숫자를 입력해주세요(1 ~ 100) : ");
 			int num = br.read();
 
-			// 초기에 입력한 숫자값과 랜덤 숫자값을 넣어줌.
-			numGame(num, randNum(), 1);
+			if (Pattern.matches("^(?:[1-9][0-9]?|100)$", String.valueOf(num))) {
+				// 초기에 입력한 숫자값과 랜덤 숫자값을 넣어줌.
+				cnt = numGame(num, 100, 1, cnt);
+			} else {
+				throw new IllegalArgumentException("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+			}
 
 		} else if(ver == 2){	//영어게임 진행
 
 			bw.write("영어를 입력해주세요(A ~ z) : ");
-			char c = (char)br.read();
+			int c = (char)br.read();
+
+			if (Pattern.matches("^[A-Za-z]+$", String.valueOf(c))) {
+				// 초기에 입력한 숫자값과 랜덤 숫자값을 넣어줌.
+				cnt = numGame(c, 100, 1, cnt);
+			} else {
+				throw new IllegalArgumentException("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+			}
 
 			// 초기에 입력한 단어값과 랜덤 단어값을 넣어줌.
-			engGame(ChartoASCII(c), randChar(), 1);
+			cnt = engGame(c, 65, 122, cnt);
 
-		} else throw new IllegalArgumentException("[ERROR] 존재하지 않는 버전입니다.");
+		} else	// 에러슈팅
+			throw new IllegalArgumentException("[ERROR] 존재하지 않는 버전입니다.");
 
-
-
-		/* error
-
-		[ERROR] 범위 내의 숫자를 입력하세요.
-		[ERROR] 범위 내의 알파벳을 입력하세요.
-		[ERROR] 입력 문자의 타입이 맞지 않습니다.
-		*/
+		bw.write("시도한 횟수 : " + cnt);
 
 		br.close();
 		bw.flush();
 		bw.close();
 	}
 
-	public static int numGame(int num, int target, int cnt) {
-		
-		// 시도한 횟수
-		return 0;
+	public static int numGame(int num, int max, int min, int cnt) throws Exception {
+		if(num > max || num < min){
+			bw.write("[ERROR] 범위 내의 숫자를 입력하세요.");
+			numGame(num, max, min, cnt++);
+		}
+
+		if(num < randNum){
+			bw.write("UP");
+			min = num;
+		} else if (num > randNum) {
+			bw.write("DOWN");
+			max = num;
+		} else if (num == randNum) {
+			bw.write("정답!");
+			return cnt;
+		}
+
+		bw.write("숫자를 입력해주세요(" + min + " ~ " + max + ") : ");
+		num = br.read();
+
+		if (Pattern.matches("^(?:[1-9][0-9]?|100)$", String.valueOf(num))) {
+			// 초기에 입력한 숫자값과 랜덤 숫자값을 넣어줌.
+			cnt = numGame(num, 100, 1, cnt);
+		} else {
+			throw new IllegalArgumentException("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+		}
+
+		return numGame(num, max, min, cnt++);
 	}
 
-	public static int engGame(int c, int target, int cnt){
+	public static int engGame(int c, int max, int min, int cnt) throws Exception{
+		if(c > max || c < min){
+			bw.write("[ERROR] 범위 내의 알파벳을 입력하세요.");
+			return engGame(c, max, min, cnt++);
+		}
 
-		// 시도한 횟수
-		return 0;
+		if(c < randChar){
+			bw.write("UP");
+			min = c;
+		} else if (c > randChar) {
+			bw.write("DOWN");
+			max = c;
+		} else if (c == randChar) {
+			bw.write("정답!");
+			return cnt;
+		}
+
+		bw.write("영어를 입력해주세요(" + min + " ~ " + max + ") : ");
+		c = (char)br.read();
+
+		if (Pattern.matches("^[A-Za-z]+$", String.valueOf(c))) {
+			// 초기에 입력한 숫자값과 랜덤 숫자값을 넣어줌.
+			cnt = numGame(c, 100, 1, cnt);
+		} else {
+			throw new IllegalArgumentException("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+		}
+
+		return engGame(c, max, min, cnt++);
 	}
 
-	// 난수 생성
-	public static int randNum(){
-		return ((int) (Math.random()*100));
-	}
-
-	// 랜덤 영어문자 생성
-	public static int randChar(){
-		return ((int)(Math.random()*52)+65);
-	}
-
-	// 아스키코드값을 단어로 바꿔주는 코드
-	public static char ASCIItoChar(int c){
-		return (char) c;
-	}
-
-	// 단어를 아스키 코드값으로 바꿔주는 코드
-	public static char ChartoASCII(int c){
-		return (char) c;
-	}
 }
