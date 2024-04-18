ae9456defb501b516ab1ee36fd1eb7b6ff7c2f8c
zzu-yaaa <jy122032@gmail.com>
2024-04-18 12:51:58 +0900

    feat: 영어ver 추가

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -7,9 +7,8 @@ public class UpdownApplication {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int tryCnt = 0;
-	static int min = 1;
-	static int max = 100;
-	static int answer = 0;
+	static int numMin = 1; static int numMax = 100; static int numAnswer = 0;
+	static char charMin = 'A'; static char charMax = 'z'; static char charAnswer;
 
 	public static void main(String[] args) throws Exception {
 
@@ -19,52 +18,51 @@ public class UpdownApplication {
 		Integer version = Integer.parseInt(br.readLine());
 		if(version == 1){
 			//숫자 버전
-			UpdownNumber();
+			updownNumber();
 		}
 		else if(version == 2){
 			//영어 버전
-			//UpdownEnglish();
+			updownEnglish();
 		}
 		else{
-			throw new Exception("[ERROR] 존재하지 않는 버전입니다.");
+			throw new IllegalArgumentException("[ERROR] 존재하지 않는 버전입니다.");
 		}
 
 	}
 
-	private static void UpdownNumber() throws IOException {
+	private static void updownNumber() throws IOException {
 		// 랜덤 정답 생성
 		Random random = new Random();
-		answer = random.nextInt(max - min + 1) + min;
+		numAnswer = random.nextInt(numMax - numMin + 1) + numMin;
 
 		//정답 맞출때까지 게임 진행
 		boolean isCorrect = false;
 		while(!isCorrect){
 			tryCnt += 1;
-			int userInput = getUserInput();
+			int userInput = getUserInputNum();
 
-			if(userInput == answer){
+			if(userInput == numAnswer){
 				System.out.println("정답!");
 				System.out.println("\n시도한 횟수 : "+tryCnt+"회");
 				isCorrect = true;
 			}
 			else{
-				checkRange(userInput);
+				checkRangeNum(userInput);
 			}
 		}
-
 	}
 
 	//입력 조건을 확인면서 입력 받기
-	private static int getUserInput() throws IOException{
+	private static int getUserInputNum() throws IOException{
 		int userInput = 0;
 		boolean isValidInput = false;
 
 		while(!isValidInput){
 			try{
-         		System.out.print("숫자를 입력해주세요(" + min + " ~ " + max + ") : ");
+         		System.out.print("숫자를 입력해주세요(" + numMin + " ~ " + numMax + ") : ");
          		userInput = Integer.parseInt(br.readLine());
 
-				 if(IsRightValue(userInput)){
+				 if(IsRightValueNum(userInput)){
 					isValidInput = true;
 				 } 
 				 else{
@@ -83,25 +81,99 @@ public class UpdownApplication {
 	}
 
 	//값이 범위 내에 있는지 확인
-	private static boolean IsRightValue(int value) {
-		if (min <= value && value <= max){
+	private static boolean IsRightValueNum(int value) {
+		if (numMin <= value && value <= numMax){
 			return true;
 		}
 		return false;
 	}
 
 	//사용자의 입력값을 판단하여 up or down 출력
-	private static void checkRange(int userInput){
-		if(userInput > answer){
+	private static void checkRangeNum(int userInput){
+		if(userInput > numAnswer){
 			System.out.println("DOWN");
-			max = userInput-1;
+			numMax = userInput-1;
 		}
-		else if(userInput < answer){
+		else if(userInput < numAnswer){
 			System.out.println("UP");
-			min = userInput+1;
+			numMin = userInput+1;
 		}
 	}
 
+	private static void updownEnglish() throws IOException {
+		// 랜덤 정답 생성
+		Random random = new Random();
+		if (random.nextBoolean()) {
+			charAnswer = (char) (random.nextInt(26) + 'A'); // 대문자 A-Z
+		} else {
+			charAnswer = (char) (random.nextInt(26) + 'a'); // 소문자 a-z
+		}
 
+		//정답 맞출때까지 게임 진행
+		boolean isCorrect = false;
+		while(!isCorrect){
+			tryCnt += 1;
+			char userInput = (char) getUserInputChar();
+
+			if(userInput == charAnswer){
+				System.out.println("정답!");
+				System.out.println("\n시도한 횟수 : "+tryCnt+"회");
+				isCorrect = true;
+			}
+			else{
+				checkRangeChar(userInput);
+			}
+		}
+	}
+
+	private static int getUserInputChar() throws IOException{
+		char userInput = 0;
+		boolean isValidInput = false;
+
+		while(!isValidInput){
+			System.out.print("알파벳을 입력해주세요 (" + charMin + " ~ " + charMax + ") : ");
+			String input = br.readLine();
+			if(input.length() == 1){
+				userInput = input.charAt(0);
+				if (userInput < 'A' || (userInput > 'z') || (userInput > 'Z' && userInput < 'a')) {
+					tryCnt += 1;
+					System.out.println("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+				}
+				else{
+					if(IsRightValueChar(userInput)){
+						isValidInput = true;
+					}
+					else{
+						tryCnt += 1;
+						System.out.println("[ERROR] 범위 내의 숫자를 입력하세요.");
+					}
+				}
+			}
+			else{
+				tryCnt += 1;
+				System.out.println("[ERROR] 한 글자만 입력하세요.");
+			}
+
+		}
+		return userInput;
+	}
+
+	private static boolean IsRightValueChar(char userInput) {
+		if(charMin <= userInput && userInput <= charMax){
+			return true;
+		}
+		return false;
+	}
+
+	private static void checkRangeChar(char userInput){
+		if(userInput > charAnswer){
+			System.out.println("DOWN");
+			charMax = (char) (userInput-1);
+		}
+		else if(userInput < charAnswer){
+			System.out.println("UP");
+			charMin = (char) (userInput+1);
+		}
+	}
 
 }
