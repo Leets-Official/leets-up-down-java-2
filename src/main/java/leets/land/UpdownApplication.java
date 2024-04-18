26f93a41c4654dbf7475d45e814e037a73b19af4
zzu-yaaa <jy122032@gmail.com>
2024-04-18 11:40:41 +0900

    feat: 숫자ver 랜덤 정답 생성 및 출력 포맷 작성

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,6 +1,8 @@
 package leets.land;
 
 import java.io.*;
+import java.util.Random;
+
 public class UpdownApplication {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,16 +15,54 @@ public class UpdownApplication {
 		Integer version = Integer.parseInt(br.readLine());
 		if(version == 1){
 			//숫자 버전
-
+			UpdownNumber();
 		}
 		else if(version == 2){
 			//영어 버전
+			//UpdownEnglish();
 		}
 		else{
-			throw new Exception("[ERROR] 올바른 버전을 선택해주세요.");
+			throw new Exception("[ERROR] 존재하지 않는 버전입니다.");
 		}
 
+	}
+
+	private static void UpdownNumber() throws IOException {
+
+		int min = 1;
+		int max = 100;
+
+		// 랜덤 정답 생성
+		Random random = new Random();
+		int answer = random.nextInt(max - min + 1) + min;
+
+		boolean isCorrect = false;
+		int tryCnt = 0;
+		while(!isCorrect){
+			int userInput = getUserInput(min, max);
+
+			if(userInput == answer){
+				System.out.println("정답!");
+				System.out.println("\n시도한 횟수 : "+tryCnt+"회");
+				isCorrect = true;
+			}
+			else{
+				checkRange(userInput);
+			}
+		}
 
 	}
+
+	//입력 조건을 확인면서 입력 받기
+	private static int getUserInput(int min, int max) {
+		return 0;
+	}
+
+	//사용자의 입력값을 판단하여 up or down 출력
+	private static void checkRange(int userInput){
+
+	}
+
+
 
 }
