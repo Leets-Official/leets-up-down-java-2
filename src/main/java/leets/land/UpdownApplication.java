6354789e3114b78a112f471763b959b8a38ae4e0
zzu-yaaa <jy122032@gmail.com>
2024-04-18 11:14:12 +0900

    feat: 게임 버전 선택

--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,8 +1,28 @@
 package leets.land;
 
+import java.io.*;
 public class UpdownApplication {
 
-	public static void main(String[] args) {
-		System.out.print("hihi :D");
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+
+		//게임 시작 & 버전 입력 받기
+		System.out.println("업다운 게임을 시작합니다.\n");
+		System.out.print("버전을 입력해주세요 (숫자 버전: 1, 영어 버전: 2) : ");
+		Integer version = Integer.parseInt(br.readLine());
+		if(version == 1){
+			//숫자 버전
+
+		}
+		else if(version == 2){
+			//영어 버전
+		}
+		else{
+			throw new Exception("[ERROR] 올바른 버전을 선택해주세요.");
+		}
+
+
 	}
+
 }
