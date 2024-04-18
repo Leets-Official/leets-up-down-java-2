a8156fa70c152bd1bea65759c963d0ea29017bf9
unknown <leejh5838@naver.com>
2024-04-18 13:38:26 +0900

    알파벳, 넘버게임 분리

---
+++ src/main/java/leets/land/AlphabetGame.java
@@ -0,0 +1,55 @@
+package leets.land;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AlphabetGame {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public char getRandomAlphabet() {
+        int randomChar = (int) (Math.random() * 52); // 0부터 51까지의 난수 생성
+        if (randomChar < 26) {
+            // 대문자 선택
+            return (char) ('A' + randomChar);
+        } else {
+            // 소문자 선택
+            return (char) ('a' + randomChar - 26);
+        }
+    }
+
+    public void run() throws Exception {
+        char answer = getRandomAlphabet();
+        int tryNum = 0;
+        char min = 'A';
+        char max = 'z';
+
+        while (true) {
+            System.out.println("영어를 입력해주세요 (" + min + " ~ " + max + ") : ");
+            char userAlphabet;
+            try {
+                userAlphabet = scanner.next().charAt(0);
+                if (!Character.isLetter(userAlphabet)) {
+                    throw new InputMismatchException("[ERROR] 문자를 입력하세요.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+                scanner.next(); // 잘못된 입력값 버퍼에서 제거
+                continue;
+            }
+
+            if (answer > userAlphabet) {
+                System.out.println("UP");
+                min = (char) (userAlphabet + 1);
+            } else if (answer < userAlphabet) {
+                System.out.println("DOWN");
+                max = (char) (userAlphabet - 1);
+            } else {
+                System.out.println("정답!");
+                break;
+            }
+            tryNum++;
+        }
+
+        System.out.println("시도한 횟수 : " + tryNum + "회");
+    }
+}
--- src/main/java/leets/land/UpdownApplication.java
+++ src/main/java/leets/land/UpdownApplication.java
@@ -1,8 +1,39 @@
 package leets.land;
 
-public class UpdownApplication {
+import java.util.Scanner;
 
+public class UpdownApplication {
 	public static void main(String[] args) {
-		System.out.print("hihi :D");
+		try {
+			int version = selectVersion();
+			if (version == 1) {
+				NumberGame numberGame = new NumberGame();
+				numberGame.run();
+			} else if (version == 2) {
+				AlphabetGame alphabetGame = new AlphabetGame();
+				alphabetGame.run();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-}
+	private static int selectVersion() throws Exception {
+		int version;
+
+		while (true) {
+			try {
+				System.out.println("버전을 선택하세요 (1 또는 2): ");
+				Scanner sc = new Scanner(System.in);
+				version = Integer.parseInt(sc.nextLine());
+				if (version != 1 && version != 2) {
+					throw new Exception("[ERROR] 존재하지 않는 버전입니다.");
+				}
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
+			}
+		}
+		return version;
+	}
+
+	}
