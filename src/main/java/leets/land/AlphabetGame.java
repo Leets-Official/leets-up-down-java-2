ce59e91bdd734bf8ac317b1dcdb3aa6ce2a3afe7
unknown <leejh5838@naver.com>
2024-05-03 12:36:17 +0900

    UserInput, compare 함수추가

--- src/main/java/leets/land/AlphabetGame.java
+++ src/main/java/leets/land/AlphabetGame.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class AlphabetGame {
     private final Scanner scanner = new Scanner(System.in);
+    private char min = 'A';
+    private char max = 'z';
 
     public char getRandomAlphabet() {
         int randomChar = (int) (Math.random() * 52); // 0부터 51까지의 난수 생성
@@ -19,36 +21,51 @@ public class AlphabetGame {
     public void run() throws Exception {
         char answer = getRandomAlphabet();
         int tryNum = 1;
-        char min = 'A';
-        char max = 'z';
 
         while (true) {
             System.out.println("영어를 입력해주세요 (" + min + " ~ " + max + ") : ");
-            char userAlphabet;
-            try {
-                userAlphabet = scanner.next().charAt(0);
-                if (!Character.isLetter(userAlphabet)) {
-                    throw new InputMismatchException("[ERROR] 문자를 입력하세요.");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("[ERROR] 입력 문자의 타입이 맞지 않습니다.");
-                scanner.next(); // 잘못된 입력값 버퍼에서 제거
-                continue;
-            }
+            char userAlphabet = UserInput();
 
-            if (answer > userAlphabet) {
-                System.out.println("UP");
-                min = (char) (userAlphabet + 1);
-            } else if (answer < userAlphabet) {
-                System.out.println("DOWN");
-                max = (char) (userAlphabet - 1);
-            } else {
+            if (compare(answer, userAlphabet)) {
                 System.out.println("정답!");
                 break;
+            } else {
+                tryNum++;
             }
-            tryNum++;
         }
 
         System.out.println("시도한 횟수 : " + tryNum + "회");
+    }
+    private char UserInput() {
+        char userAlphabet;
+        while (true) {
+            try {
+                userAlphabet = scanner.next().charAt(0);
+                if (!Character.isLetter(userAlphabet)) {
+                    throw new Exception("[ERROR] 문자를 입력하세요.");
+                }
+                if (userAlphabet < min || userAlphabet > max) {
+                    throw new Exception("[ERROR] 범위 내의 문자를 입력하세요.");
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                System.out.println("영어를 입력해주세요 (" + min + " ~ " + max + ") : ");
+                scanner.nextLine();
+            }
+        }
+        return userAlphabet;
+    }
+    private boolean compare(char answer, char userAlphabet) {
+        if (answer > userAlphabet) {
+            System.out.println("UP");
+            min = (char) (userAlphabet + 1);
+        } else if (answer < userAlphabet) {
+            System.out.println("DOWN");
+            max = (char) (userAlphabet - 1);
+        } else {
+            return true;
+        }
+        return false;
     }
 }
